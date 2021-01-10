f050cacb5c0aea3e7c36c716e0ff4065196d6445
JamieDawson <jamierobertdawson@gmail.com>
2021-01-09 16:04:37 -0800

    creating button for users to post comments

--- src/Post.js
+++ src/Post.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {db} from './firebase';
 import './Post.css';
 import Avatar from '@material-ui/core/Avatar';
 
-function Post({username, caption, imageUrl}) {
+function Post({postId, username, caption, imageUrl}) {
+	const [comments, setComments] = useState([]);
+	const [comment, setComment] = useState('');
+
+	useEffect(() => {
+		let unsubscribe;
+		if (postId) {
+			unsubscribe = db
+				.collection('posts')
+				.doc(postId)
+				.collection('comments')
+				.onSnapshot((snapshot) => {
+					setComments(snapshot.docs.map((doc) => doc.data()));
+				});
+		}
+
+		return () => {
+			unsubscribe();
+		};
+	}, [postId]);
+
+	const postComment = (event) => {};
+
 	return (
 		<div className='post'>
 			<div className='post__header'>
@@ -17,9 +40,26 @@ function Post({username, caption, imageUrl}) {
 
 			<img className='post__image' src={imageUrl} alt='' />
 			<h4 className='post__text'>
-				<strong>JamieDawson:</strong> {caption}
+				<strong>{username}</strong> {caption}
 			</h4>
-			{/* username + caption */}
+
+			<form className='post__commentBox'>
+				<input
+					className='post__input'
+					type='text'
+					placeholder='add a comment'
+					value={comment}
+					onChange={(e) => setComment(e.target.value)}
+				/>
+				<button
+					className='post__button'
+					disabled={!comment}
+					type='submit'
+					onClick={postComment}
+				>
+					POST
+				</button>
+			</form>
 		</div>
 	);
 }
