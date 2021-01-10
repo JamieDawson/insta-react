028157fd8e2903d206a23fa9c24617a6d5bfb52c
JamieDawson <jamierobertdawson@gmail.com>
2021-01-09 19:54:48 -0800

    comments now appearing

--- src/Post.js
+++ src/Post.js
@@ -43,6 +43,14 @@ function Post({postId, username, caption, imageUrl}) {
 				<strong>{username}</strong> {caption}
 			</h4>
 
+			<div className='post__comments'>
+				{comments.map((comment) => (
+					<p>
+						<strong>{comment.username}</strong> {comment.text}
+					</p>
+				))}
+			</div>
+
 			<form className='post__commentBox'>
 				<input
 					className='post__input'
